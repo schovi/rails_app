63200bd06ee3f38b925856b2a7055737f28292c2
Erich Kaderka <erich.kaderka@gmail.com>
2014-06-23 13:51:28 +0200

    Initial commit with basic structure for cookbook

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# 0.1.0
+
+Initial release of rubygems_app
+
+* Enhancements
+  * an enhancement
+
+* Bug Fixes
+  * a bug fix
---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'rubygems_app'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures rubygems_app'
+long_description 'Installs/Configures rubygems_app'
+version          '0.1.0'
+
