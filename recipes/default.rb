79f9f026ecdca401860a07813b790fd0ca73e399
Erich Kaderka <erich.kaderka@gmail.com>
2014-06-24 07:02:28 +0200

    Add nginx + unicorn to Cheffile and to basic recipes

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,8 +7,12 @@
 #
 #
 
+include_recipe "nginx"
 
-user_account 'rubygems_app' do
+node['default']['app_name'] = 'rubygems_app'
+node['default']['domain'] = "#{node['default']['app_name']}.com"
+
+user_account node['default']['app_name'] do
   action :create
 end
 
@@ -16,13 +20,44 @@ package 'git' do
   action :install
 end
 
-application 'my-app' do
+
+application node['default']['app_name'] do
   path '/home/rubygems_app'
   repository 'https://github.com/rubygems/rubygems.org.git'
   revision 'master'
 end
 
 #unicorn
+
+node.default[:unicorn][:worker_timeout] = 60
+node.default[:unicorn][:preload_app] = false
+node.default[:unicorn][:worker_processes] = [node[:cpu][:total].to_i * 4, 8].min
+node.default[:unicorn][:preload_app] = false
+node.default[:unicorn][:before_fork] = 'sleep 1'
+node.default[:unicorn][:port] = '8080'
+node.set[:unicorn][:options] = { :tcp_nodelay => true, :backlog => 100 }
+
+unicorn_config "/etc/unicorn/#{app['id']}.rb" do
+  listen({ node[:unicorn][:port] => node[:unicorn][:options] })
+  working_directory ::File.join(app['deploy_to'], 'current')
+  worker_timeout node[:unicorn][:worker_timeout]
+  preload_app node[:unicorn][:preload_app]
+  worker_processes node[:unicorn][:worker_processes]
+  before_fork node[:unicorn][:before_fork]
+end
+
+#nginx
+
+cookbook_file "#{node['nginx']['dir']}/sites-available/#{node['default']['domain']}" do
+  owner "root"
+  group "root"
+  mode  "0644"
+end
+
+# enable your sites configuration using a definition from the nginx cookbook
+nginx_site node['default']['domain'] do
+  enable true
+end
 
 
 
