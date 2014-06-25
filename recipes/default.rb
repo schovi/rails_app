20bb1a499396d9d0182cf60b1d48e26670899667
Erich Kaderka <erich.kaderka@gmail.com>
2014-06-25 06:18:24 +0200

    Change attributes - problem with e.g. path default['rubygems_app']['name'] -> chef thought of it as an array not key hash, it resulted into TypeError: can't convert String into Integer

--- recipes/default.rb
+++ recipes/default.rb
@@ -10,7 +10,7 @@
 include_recipe "nginx"
 
 
-user_account node['rubygems_app']['name'] do
+user_account node['name'] do
   action :create
 end
 
@@ -19,8 +19,8 @@ package 'git' do
 end
 
 
-application node['rubygems_app']['name'] do
-  path ['node']['rubygems_app']['application_path']
+application node['name'] do
+  path ['node']['application_path']
   repository 'https://github.com/erich/simple-rails.git'
   revision 'master'
 end
@@ -30,7 +30,7 @@ rvm_line = '[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
 ruby_block "add rvm basrhc config line" do
   block do
     #NOTE move this to attributes
-    file = Chef::Util::FileEdit.new("#{['node']['rubygems_app']['basrhc']}")
+    file = Chef::Util::FileEdit.new("#{['node']['basrhc']}")
     file.insert_line_if_no_match(rvm_line, rvm_line)
     file.write_file
   end
@@ -38,20 +38,20 @@ end
 
 ruby_block "reload .bashrc" do
   block do
-    Chef::Config.from_file("#{['node']['rubygems_app']['basrhc']}")
+    Chef::Config.from_file("#{['node']['basrhc']}")
   end
   action :nothing
 end
 
 execute "cd to app directory, run bundle install and restart unicorn" do
-  user ['node']['rubygems_app']
+  user ['node']
   #NOTE split to mulitple lines maybe 2 execute
-  command "cd #{['node']['rubygems_app']['application_path']} && bundle install && bundle exec unicorn -c #{['node']['unicorn']['config_file']}"
+  command "cd #{['node']['application_path']} && bundle install && bundle exec unicorn -c #{['node']['unicorn']['config_file']}"
 end
 
 unicorn_config "#{['node']['unicorn']['config_file']}" do
   listen({ node[:unicorn][:port] => node[:unicorn][:options] })
-  working_directory ::File.join(node['rubygems_app']['name'], 'current')
+  working_directory ::File.join(node['name'], 'current')
   worker_timeout node[:unicorn][:worker_timeout]
   preload_app node[:unicorn][:preload_app]
   worker_processes node[:unicorn][:worker_processes]
@@ -60,23 +60,23 @@ end
 
 #nginx -> it should have its own cookbook
 
-file "#{node['nginx']['dir']}/sites-available/#{node['rubygems_app']['domain']}" do
-  owner node['rubygems_app']['name']
+file "#{node['nginx']['dir']}/sites-available/#{node['domain']}" do
+  owner node['name']
   mode  "0644"
 end
 
-template "/etc/nginx/sites-available/#{node['rubygems_app']['domain']}" do
+template "/etc/nginx/sites-available/#{node['domain']}" do
     source "nginx_site.erb"
     mode 644
     variables(
-      :application_name => node['rubygems_app']['domain'],
-      :application_path => node['rubygems_app']['application_path'],
+      :application_name => node['domain'],
+      :application_path => node['application_path'],
       :unicorn_port => node['unicorn']['port']
     )
   end
 
 # enable your sites configuration using a definition from the nginx cookbook
-nginx_site node['rubygems_app']['domain'] do
+nginx_site node['domain'] do
   enable true
 end
 
