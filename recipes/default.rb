17d8ddb33a837050a2d43b2f2f634b84386c7cb5
Erich Kaderka <erich.kaderka@gmail.com>
2014-06-25 19:49:47 +0200

    Split default recipe to 2. RVM must be installed between after application structure and before rvm_shell

--- recipes/default.rb
+++ recipes/default.rb
@@ -10,44 +10,6 @@
 include_recipe "nginx"
 
 
-user_account node['name'] do
-  action :create
-end
-
-package 'git' do
-  action :install
-end
-
-#it helps with debugging
-package 'tmux' do
-  action :install
-end
-
-#Fix Could not find a JavaScript runtime. See https://github.com/sstephenson/execjs for a list of available runtimes. (ExecJS::RuntimeUnavailable) on Debian - starting rails app
-package 'libv8-dev' do
-  action :install
-end
-
-#Install development headers for mysql required by mysql2 gem
-
-package 'libmysqlclient-dev' do
-  action :install
-end
-
-application node['name'] do
-  owner 'rubygems_app'
-  group 'rubygems_app'
-  path '/home/rubygems_app'
-  repository 'https://github.com/erich/simple-rails.git'
-  revision 'master'
-  rails do
-    database do
-      database "rubygems_app_production"
-      username "rubygems_app"
-      password "12*asda!BanqV"
-    end
-  end
-end
 
 rvm_shell "bundle" do
   ruby_string 'ruby-2.0.0@rubygems_app'
